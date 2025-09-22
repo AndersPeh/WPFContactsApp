75a84954df612f9b5094aac7abe03ff81677d53b
AndersPeh <pehjyfung@gmail.com>
2025-09-22 17:02:29 +1000

    Add project files.

---
+++ WPFContactsApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            // ShowDialog forbids user from interacting with the main window unless the newContactWindow is closed.
+            // Show allows user to interacting with the main window when the newContactWindow is opened.
+            newContactWindow.ShowDialog();
+        }
+    }
+}
---
+++ WPFContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SQLite;
+using WPFContactsApp.Classes;
+
+namespace WPFContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text,
+            };
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+
+            // using disposes SQLiteConnection after the execution leaves the block of code in {}.
+            // After connection.Insert(contact), SQLiteConnection will be disposed.
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            {
+                // This will be ignored if the table has been created.
+                connection.CreateTable<Contact>();
+
+                // SQLiteConnection will detect contact belongs to Contact table automatically and insert contact to Contact table.
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
